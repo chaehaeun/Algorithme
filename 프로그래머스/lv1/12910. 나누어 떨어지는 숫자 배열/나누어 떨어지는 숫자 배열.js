561ba4ce8023e5190d442ab1138e724be35ff595
Haeun Chae <chae_3210@naver.com>
2022-11-18 12:51:44 +0900

    풀이 코드 업데이트

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,23 +1,10 @@
 function solution(arr, divisor) {
-    let answer = [];
-    
-    for(let i = 0; i < arr.length; i++){
-        if (arr[i] % divisor == 0){
-            answer.push(arr[i]);
-        }  
+  let myArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      myArr.push(arr[i]);
     }
-    if (answer.length === 0){
-            return [-1];
-        }
-    
-    answer.sort((a, b) => {return a - b});
-    return answer;
-    
-    
+  }
 
- // answer = arr.filter(v => v%divisor == 0);
- // return answer.length == 0 ? [-1] : answer.sort((a,b) => a-b);
-    
-
-
+  return myArr.length === 0 ? (myArr = [-1]) : myArr.sort((a, b) => a - b);
 }
