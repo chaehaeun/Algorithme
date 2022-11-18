6bd299560ec5ba1976941a2359d1a054301d8040
Chae <103991310+chaehaeun@users.noreply.github.com>
2022-11-18 23:54:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.12 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,8 @@
+function solution(arr){
+    const newArr = [];
+    for(let i = 0 ; i < arr.length ; i++ ){
+        if (arr[i] !== arr[i+1]) newArr.push(arr[i]);
+    }
+    
+    return newArr;
+}
