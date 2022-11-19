bf038c7f7011249b640586510c8fdb0c48966642
Chae <103991310+chaehaeun@users.noreply.github.com>
2022-11-19 16:04:12 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,20 +1,10 @@
 function solution(s) {
-    let arr = s.split(' '); // 문자열 쪼개서 배열로 만들었고
-    let result = ''
-    
-    console.log(arr)
-    
-    for (let i = 0 ; i < arr.length; i++){
-        for (let j = 0 ; j < arr[i].length; j++){  
-          j % 2=== 0 ? result += arr[i][j].toUpperCase() :  result += arr[i][j].toLowerCase();
-        }
-        
-        // 단어 하나가 다 처리 되고 나면, 공백을 하나 넣어줘야 됨
-        if(i !== arr.length - 1){
-            result += ' '
-        }
-
-    }
-    
-    return result
+  let answer = "";
+  let cnt = -1;
+  for (let i = 0; i < s.length; i++) {
+    s[i] === " " ? (cnt = -1) : cnt++;
+    answer += (cnt % 2 === 0 ? s[i].toUpperCase() : s[i]);
+    // console.log(s[i], cnt, answer[i]);
+  }
+  return answer;
 }
