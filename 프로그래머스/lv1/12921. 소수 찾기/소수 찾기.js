3f90f51ffb6734a9cae39be1d532d0b21fced6ea
Chae <103991310+chaehaeun@users.noreply.github.com>
2022-11-20 18:06:59 +0900

    [level 1] Title: 소수 찾기, Time: 17.17 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,26 @@
+
+
+function solution(n) {
+    let answer = 0;
+    const arr = new Array(n+1).fill(true); // 초깃값 설정
+    
+    for(let i = 2; i <= Math.sqrt(n) ; ++i){
+        // 이미 소수가 아닌 인덱스는 건너뛴다.
+        if(arr[i] === false){
+            continue; 
+        }
+        // 소수가 아닌 데이터는 false로 입력한다.
+        for(let k = i * i; k <= n; k += i){
+            arr[k] = false;
+        }
+    }
+    // 소수의 갯수를 구한다.
+    for(let i = 2; i <= n; ++i){
+        if(arr[i] === true){
+            answer++;
+        }
+    }
+    return answer;
+    
+    
+}
