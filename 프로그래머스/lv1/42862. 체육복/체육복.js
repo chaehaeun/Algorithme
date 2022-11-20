6b8e89fb576a29d83397ceb11e133cbf2cafade6
Chae <103991310+chaehaeun@users.noreply.github.com>
2022-11-21 00:59:02 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,25 +1,25 @@
 function solution(n, lost, reserve) {
-     // 체육복을 잃어버렸으면서 여벌이 없는 경우(0개인 친구)
-    let clothesLost = lost.sort((a,b)=> a-b).filter((lost) => !reserve.includes(lost));
+    const students = {};
+    let answer = 0;
+    for(let i = 1; i <= n; i++){
+        students[i] = 1;
+    }
+    lost.forEach(number => students[number] -= 1);
+    reserve.forEach(number => students[number] += 1);
 
-    // 여벌이 있으면서 체육복을 잃어버리지 않은 경우(2개인 친구)
-    let clothesReserve = reserve.sort((a,b)=> a-b).filter((reserve) => !lost.includes(reserve));
-    
-    
-     return (
-        // realLost.filter((lost) : 잃어버린 사람 중 여벌을 받을 수 없는 학생을 배열로 반환
-        n -
-        clothesLost.filter((lost) => {
-            // lend : 체육복을 빌려줄 수 있는 경우 중 첫 번째 요소
-            const lend = clothesReserve.find(
-                // 잃어버린 사람의 1칸 주위에 빌릴 사람이 있는 경우
-                (reverse) => Math.abs(reverse - lost) == 1,
-            );
-
-            // 빌려줄 수 있는 사람이 없으면 진짜 잃어버린 걸로 간주하고 lost를 return
-            if (!lend) return lost;
-            // 빌려 줬으면 reserve 배열에서 빌려준 사람 제외
-            clothesReserve = clothesReserve.filter((reverse) => reverse !== lend);
-        }).length
-    );
+    for(let i = 1; i <= n; i++){
+        if(students[i] === 2 && students[i-1] === 0){
+                students[i-1]++;
+                students[i]--;
+        } else if(students[i] === 2 && students[i+1] === 0){
+                students[i+1]++;
+                students[i]--;
+        }
+    }
+    for(let key in students){
+        if(students[key] >= 1){
+            answer++;
+        }
+    }
+    return answer;
 }
