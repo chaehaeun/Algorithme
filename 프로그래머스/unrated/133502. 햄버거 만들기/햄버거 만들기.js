c1484c844f9e8891d50e4b6db0f363520a12777f
Chae <103991310+chaehaeun@users.noreply.github.com>
2022-11-29 22:49:42 +0900

    [unrated] Title: 햄버거 만들기, Time: 3.79 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,24 +1,13 @@
 function solution(ingredient) {
+    const hamburger = '1231';
     let count = 0;
-    const arr = [];
     
     for(let i = 0; i < ingredient.length; i++){
-        const len = arr.push(ingredient[i]);
-        // push()는 배열 끝에 요소를 추가하고 배열의 길이를 반환함
-        if(len >= 4){
-            if(
-                arr[len - 4] === 1 &&
-                arr[len - 3] === 2 &&
-                arr[len - 2] === 3 &&
-                arr[len - 1] === 1
-            ){
-                arr.pop();
-                arr.pop();
-                arr.pop();
-                arr.pop();         
-                
-                count++;
-            }
+        if(ingredient.slice(i, i + 4).join('') === hamburger){
+            ingredient.splice(i,4);
+            count++  
+            console.log(i)
+            i-=3
         }
     }
     return count;
