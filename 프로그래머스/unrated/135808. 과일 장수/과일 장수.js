3334c4409da3e66282a25576982f002e19905192
Chae <103991310+chaehaeun@users.noreply.github.com>
2022-11-30 20:49:38 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,13 @@
+function solution(k, m, score) {
+    let answer = 0;
+    score.sort((a,b) => b-a);
+    
+    let idx = m-1;
+
+    for(let i = 0; i < Math.floor(score.length/m); i++){
+        answer += score[idx]*m;
+        
+        idx+= m;
+    }
+    return answer;
+}
