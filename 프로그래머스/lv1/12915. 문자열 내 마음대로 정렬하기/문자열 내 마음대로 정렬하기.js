0783cfdf3e52e2096fee33235ac97261d9500aed
Chae <103991310+chaehaeun@users.noreply.github.com>
2023-01-15 11:44:00 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,10 +1,23 @@
 function solution(strings, n) {
-    strings.sort();
-    strings.sort((a, b) => {
-        if(a[n]!==b[n]){
-            return a[n] < b[n] ? -1 : 1;
-        }   
-    });
-  
-    return strings; 
+  strings.sort();
+  strings.sort((a, b) => {
+    if (a[n] !== b[n]) {
+      return a[n] < b[n] ? -1 : 1;
+    }
+  });
+
+  return strings;
 }
+
+// function solution(strings, n) {
+//   return strings.sort((a, b) => {
+//     if (a[n] > b[n]) return 1;
+//     else if (a[n] < b[n]) return -1;
+//     //문자가 같으면 사전순
+//     else return a > b ? 1 : -1;
+//   });
+// }
+
+// 이 함수가 리턴하는 값이 0보다 작을 경우,  a가 b보다 앞에 오도록 정렬하고,
+// 이 함수가 리턴하는 값이 0보다 클 경우, b가 a보다 앞에 오도록 정렬합니다.
+// 만약 0을 리턴하면, a와 b의 순서를 변경하지 않습니다.
