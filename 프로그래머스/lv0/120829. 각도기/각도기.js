49a00c03b05f04544ac10ee0f07c5515a4a2ed2b
Chae <103991310+chaehaeun@users.noreply.github.com>
2023-02-13 23:09:51 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,6 +1,3 @@
 function solution(angle) {
-    if (angle < 90 ) return 1;
-    if (angle === 90) return 2;
-    if (angle > 90 && angle < 180) return 3;
-    if (angle === 180) return 4;
+    return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
 }
