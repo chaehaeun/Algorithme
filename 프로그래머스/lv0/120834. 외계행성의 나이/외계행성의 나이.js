1048508106fe27b42922e2916f995d67ba8e9ff3
Chae <103991310+chaehaeun@users.noreply.github.com>
2023-04-16 17:20:35 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,16 @@
+function solution(age) {
+    const ageObj = {
+        0 : "a",
+        1 : "b",
+        2 : "c",
+        3 : "d",
+        4 : "e",
+        5 : "f",
+        6 : "g",
+        7 : "h",
+        8 : "i",
+        9 : "j"
+    }
+
+    return age.toString().split('').map(num => ageObj[num]).join('')
+}
