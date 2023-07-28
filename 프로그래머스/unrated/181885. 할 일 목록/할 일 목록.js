f3426ac12f569ac5d678316d86e1c5c5e890b8c9
Chae <103991310+chaehaeun@users.noreply.github.com>
2023-07-28 22:43:22 +0900

    [unrated] Title: 할 일 목록, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,11 @@
+function solution(todo_list, finished) {
+    let answer = [];
+    
+    for(let i = 0; i < todo_list.length; i++){
+        if(!finished[i]){
+            answer.push(todo_list[i]);
+        }
+    }
+    
+    return answer;
+}
