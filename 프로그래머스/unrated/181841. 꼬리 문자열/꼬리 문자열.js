38cd16295e656bb7b47397fa93ee0d89e93381ab
Chae <103991310+chaehaeun@users.noreply.github.com>
2023-09-27 21:39:36 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,9 @@
+function solution(str_list, ex) {
+    let answer = ''
+    for(let i = 0; i < str_list.length; i++){
+        if(str_list[i].includes(ex)) continue;
+        answer+=str_list[i];
+    }
+    
+    return answer;
+}
